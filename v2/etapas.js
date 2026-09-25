f9293d51d0f0560f2e657e38c5f2951370cf974e
Leo Lemos <leo@lemos.dev>
2026-09-25 16:33:05 -0300

    Versão 2: Adicionados botões de tela cheia para celular e tablet

---
+++ v2/etapas.js
@@ -0,0 +1,86 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '2200',
+                nome: 'CARMELO NETO',
+                partido: 'PL',
+                fotos: [
+                    { url: 'carmelo.png' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '22026',
+                nome: 'ANANDA NORDESTINA',
+                partido: 'PL',
+                fotos: [
+                    { url: 'ananda.png' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'SENADOR 1',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '222',
+                nome: 'ALCIDES FERNANDES',
+                partido: 'PL',
+                fotos: [
+                    { url: 'alcides.jpg' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'SENADOR 2',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '445',
+                nome: 'CAPITÃO WAGNER',
+                partido: 'NOVO',
+                fotos: [
+                    { url: 'capitao_wagner.jpg' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '45',
+                nome: 'CIRO GOMES',
+                partido: 'PSDB',
+                fotos: [
+                    { url: 'ciro.png' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'FLÁVIO BOLSONARO',
+                partido: 'PL',
+                fotos: [
+                    { url: 'flavio.jpg' }
+                ]
+            }
+        ]
+    }
+];
